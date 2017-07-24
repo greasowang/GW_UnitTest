2f8a3e0babc81826a16ef0670e4e81b36e035591
Greaso Wang <wang121012@gmail.com>
2017-07-24 18:09:42 +0800

    Initial Commit

---
+++ GWUnitTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GWUnitTestProject
+//
+//  Created by Greaso Wang on 2017/7/24.
+//  Copyright © 2017年 Greaso Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
